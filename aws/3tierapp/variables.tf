aed7a1273570adfc03d1a725f617440620cda9a4
priyamchauhan06 <65408954+priyamchauhan06@users.noreply.github.com>
2023-01-22 13:30:44 +0530

    Update variables.tf

--- aws/3tierapp/variables.tf
+++ aws/3tierapp/variables.tf
@@ -1,9 +1,3 @@
-variable "aws_access_key" {
-  description = "aws access key"
-}
-variable "aws_secret_key" {
-  description = "aws secret key"
-}
 variable "vpC_cidr_range" {
   description = "IP Range of vpc to be created"
   default     = "10.0.0.0/16"
