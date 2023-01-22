3d2ccbf2822f0ec8451021d3751186745292a39f
priyamchauhan06 <65408954+priyamchauhan06@users.noreply.github.com>
2023-01-22 14:21:57 +0530

    Update variables.tf

--- aws/3tierapp/variables.tf
+++ aws/3tierapp/variables.tf
@@ -1,6 +1,12 @@
 variable "region" {
   description = "aws region in which deployment has to be done"
 }
+variable "aws_access_key" {
+  description = "aws access key"
+}
+variable "aws_secret_key" {
+  description = "aws secret key"
+}
 
 variable "vpC_cidr_range" {
   description = "IP Range of vpc to be created"
