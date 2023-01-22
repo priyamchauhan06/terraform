399a6fc9e3b49c3ca49e7d125a288e5855c2e3c4
priyamchauhan06 <65408954+priyamchauhan06@users.noreply.github.com>
2023-01-22 23:59:19 +0530

    Update main.tf

--- aws/sample/vpc/main.tf
+++ aws/sample/vpc/main.tf
@@ -60,3 +60,22 @@ resource "aws_route_table_association" "pub-subnet-associatn" {
   subnet_id      = aws_subnet.publc_subnet.id
   route_table_id = aws_default_route_table.def_route_table.id
 }
+
+#elastic ip creation
+resource "aws_eip" "eip" {
+  vpc      = true
+}
+
+#NAT gateway creation
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.publc_subnet.id
+
+  tags = {
+    Name = "dev-vpc-nat-gw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
