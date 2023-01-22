05a22d182a2fcd5019686cad57451cd10a5ecd91
priyamchauhan06 <65408954+priyamchauhan06@users.noreply.github.com>
2023-01-23 00:06:49 +0530

    nat gatewat rt updated

--- aws/sample/vpc/main.tf
+++ aws/sample/vpc/main.tf
@@ -79,3 +79,17 @@ resource "aws_nat_gateway" "nat_gw" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
 }
+
+#NAT route table creation
+resource "aws_route_table" "nat_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "dev-vpc-natgw-rt"
+  }
+}
