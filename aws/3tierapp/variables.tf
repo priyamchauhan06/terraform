bad2dd48048cc71c05c55ee96f674e49b119f485
priyamchauhan06 <65408954+priyamchauhan06@users.noreply.github.com>
2023-01-22 13:57:13 +0530

    Update variables.tf
    aws region variable updated

--- aws/3tierapp/variables.tf
+++ aws/3tierapp/variables.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  description = "aws region in which deployment has to be done"
+}
+
 variable "vpC_cidr_range" {
   description = "IP Range of vpc to be created"
   default     = "10.0.0.0/16"
